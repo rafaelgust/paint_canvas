903c62b6716d40a34dcbd515d3632c873eb70ae4
rafaelgust <rafael.appdevs@gmail.com>
2021-12-02 19:06:04 -0300

    starting

---
+++ js/canvas.js
@@ -0,0 +1,22 @@
+const canvas = document.querySelector('#paint');
+const ctx = canvas.getContext('2d');
+
+window.addEventListener("load", () => {
+    canvas.width = window.innerWidth - ((window.innerWidth / 100) * 20);
+    canvas.height = window.innerHeight - ((window.innerHeight / 100) * 20);
+});
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth - ((window.innerWidth / 100) * 20);
+    canvas.height = window.innerHeight - ((window.innerHeight / 100) * 20);
+});
+
+let painting = false;
+
+function startClick(){
+    painting = true;
+}
+
+function closeClick(){
+    painting = false;
+}
